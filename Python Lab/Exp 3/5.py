445e96becbd2db0cc2f4956b4ab3281f389565fd
ayush-sah <ayush.sah@spit.ac.in>
2020-01-27 23:37:30 +0530

    update

---
+++ Python Lab/Exp 3/5.py
@@ -0,0 +1,13 @@
+# Create one dictionary of 5 students with their name, address, age, class and marks of 5 subjects. Perform all the
+# operations on the created dictionary
+
+students = {[1, 2, 3, 4, 5]: {'name': ['Ayush', 'Dhiraj', 'Sumit', 'Sanket', 'Nikhil'],
+                              'address': ['Colaba', 'Andheri', 'Vile Parle', 'Powai', 'Goregaon'],
+                              'age': [20, 19, 21, 17, 18], 'class': ['MCA', 'MBA', 'BCA', 'BBA', 'B.Tech'],
+                              'marks': [[92, 95, 94, 98, 93], [94, 90, 93, 97, 95], [90, 85, 91, 93, 92],
+                                        [88, 84, 83, 81, 89], [82, 85, 84, 88, 83]]}}
+#repeat = True
+#while repeat:
+#    action = input("1. Insert\n2. Update\n3. Delete\n4. Exit\nEnter Action you want to perform: ")
+#    if action == 1:
+#        students[len(students[1])] = len(students[1])
