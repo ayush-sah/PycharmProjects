59fb1043e5fc0d01e4e91d88b485a3e27e8a9016
ayush-sah <58354732+ayush-sah@users.noreply.github.com>
2020-02-03 16:01:23 +0530

    1.py

---
+++ Python Lab/Exp 4/1.py
@@ -0,0 +1,29 @@
+repeat = True
+while repeat:
+
+    string = input("Enter a String: ")
+
+    def palindrome(i, j, check=True):
+        if i < j:
+
+            if (string[i] != string[j]):
+                return False
+
+            else:
+                i += 1
+                j -= 1
+                return palindrome(i, j)
+        else:
+            return True
+
+    check = palindrome(0,len(string)-1)
+    if check:
+        print("Palindrome")
+    else:
+        print("Not a Palindrome")
+
+    repeat = input("\nCheck another string ('Y','N'):  ")
+    if repeat in ['y', 'Y', 'yes', 'Yes', 'YES']:
+        continue
+    else:
+        repeat = False
