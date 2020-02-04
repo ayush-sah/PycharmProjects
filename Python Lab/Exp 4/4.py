ae8db229fc2e4a8ccc4ea8fa39d74dcc9c799e71
ayush-sah <58354732+ayush-sah@users.noreply.github.com>
2020-02-04 12:46:33 +0530

    Delete 4.py

--- Python Lab/Exp 4/4.py
+++
@@ -1,6 +0,0 @@
-l = [1,2,3,4,5]
-addition = map(lambda x: x+x, l)
-square = map(lambda x: x*x, l)
-
-print("After addition values are: ", list(addition))
-print("After multiplication values are: ", list(square))
