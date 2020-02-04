da8a01b8e596869556c38ac7a4d6ae525678d6d2
ayush-sah <58354732+ayush-sah@users.noreply.github.com>
2020-02-04 11:45:28 +0530

    Delete file1.py

--- Python Lab/Exp 4/file1.py
+++
@@ -1,9 +0,0 @@
-def calculator(action):
-    if action[1] == '+':
-        return int(action[0]) + int(action[2])
-    elif action[1] == '-':
-        return int(action[0]) - int(action[2])
-    elif action[1] == '*':
-        return int(action[0]) * int(action[2])
-    elif action[1] == '/':
-        return int(action[0]) / int(action[2])
