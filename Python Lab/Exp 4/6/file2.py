0fb9ef15a0438fa079a35a38833d45609554c5cc
ayush-sah <58354732+ayush-sah@users.noreply.github.com>
2020-02-04 13:01:03 +0530

    Update file2.py

--- Python Lab/Exp 4/6/file2.py
+++ Python Lab/Exp 4/6/file2.py
@@ -1,4 +1,4 @@
 from file1 import calculator
 
-action = input("Enter calculator function: ").split()
+action = input("Enter operater and operands(add spaces): ").split()
 print("Answer: ", calculator(action))
